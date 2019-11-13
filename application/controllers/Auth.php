10afc4f3850ffaeefe62f797b515c91315e9aee1
meirusfandi <meirusfandi100@gmail.com>
2019-11-14 05:05:31 +0700

    update auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,11 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    public function __construct()
-    {
+
+    function __construct()
+	{
         parent::__construct();
         $this->load->library('form_validation');
-    }
+
+		date_default_timezone_set('Asia/Jakarta');
+
+		$this->load->model('Admin_models');
+
+		// cek session yang login, 
+		// jika session status tidak sama dengan session telah_login, berarti pengguna belum login
+		// maka halaman akan di alihkan kembali ke halaman login.
+		if($this->session->userdata('status')!="telah_login"){
+			redirect(base_url().'login?alert=belum_login');
+		}
+	}
 
     public function index()
     {
