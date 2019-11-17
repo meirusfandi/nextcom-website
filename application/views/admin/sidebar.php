5552c8a62fb0a7229ca9625319c3dbc55c62e5e0
meirusfandi <meirusfandi100@gmail.com>
2019-11-17 07:12:45 +0700

    update

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -1,10 +1,12 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-  <!-- Brand Logo -->
-  <a href="index3.html" class="brand-link">
-    <img src="<?php echo base_url("assets/"); ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">AdminLTE 3</span>
-  </a>
+    <!-- Brand Logo -->
+    <a href="<?php echo base_url('admin'); ?>" class="brand-link">
+      <img src="<?php echo base_url("assets/");?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
@@ -15,11 +17,12 @@
         <div class="info">
           <a href="#" class="d-block"><?php echo $profile->nama; ?></a>
         </div>
+      </div>
 
-    <!-- Sidebar Menu -->
-    <nav class="mt-2">
-      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li  class="nav-item">
             <a href="<?php echo base_url('admin'); ?>" <?php if ($page == "dashboard") { ?> class="active nav-link" <?php } else { ?> class="nav-link" <?php } ?>>
@@ -74,12 +77,12 @@
               <i class="nav-icon fas fa-th"></i>
               <p>
               Logout
-            </p>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <!-- /.sidebar-menu -->
-  </div>
-  <!-- /.sidebar -->
-</aside>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
